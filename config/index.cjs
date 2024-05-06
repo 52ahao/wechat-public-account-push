2dacf169106085dabb0431a8c6335ac76874edce
52ahao <102865359+52ahao@users.noreply.github.com>
2024-05-06 22:20:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -113,7 +113,7 @@ const USER_CONFIG = {
       id: 'o8cRk6cSE3VVG5Pd_SxbslSxCQCc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: '8Q7C5ANbcsOu3OM-AQmiLWsKL6081U3r1fWrITHuVYE',
+      useTemplateId: 'YXiaCi1gnfEPlNVfP57_7_m_kuZtuob5xV5_h_u-Hgs',
       // 所在省份或城市，也可以不填
       province: '甘肃',
       // 所在城市或县区
